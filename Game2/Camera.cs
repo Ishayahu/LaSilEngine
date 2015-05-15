6c9f1c5fc4adc46d0e4c3cb42a27058d262240ff
Ishayahu <ishayahu@mail.ru>
2015-05-15 12:36:00 +0300

    Проверка push

---
+++ Game2/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game2
+{
+    /// <summary>
+    /// Класс, отвечающий за камеру. Он обеспечивает отслеживание её координат, перемещение и направление
+    /// Видимость обрабатывается в движке
+    /// </summary>
+    public class Camera
+    {
+        public Camera()
+            : base()
+        {
+
+        }
+    }
+}
